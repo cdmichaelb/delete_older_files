98c2166af4597012f865d8a6aca753a30907b43c
cdmichaelb <broetjem@gmail.com>
2021-11-14 05:40:54 -0800

    saving this for future projects

---
+++ code_pad.py
@@ -0,0 +1,20 @@
+    """Code I'm saving for later.
+    """
+import ctypes
+
+def get_is_admin():
+    """Get the UAC level.
+    """
+    try:
+        return ctypes.windll.shell32.IsUserAnAdmin()
+    except:
+        return "Could not get the UAC level."
+    
+def set_admin():
+    """Set the UAC level to admin.
+    """
+    try:
+        ctypes.windll.shell32.ShellExecuteW(None, "runas", sys.executable, __file__, None, 1)
+    except:
+        print("Could not set the UAC level.")
+
